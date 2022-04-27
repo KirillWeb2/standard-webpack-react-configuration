0187cd19f4c99a7500ee87299f418b0205e9f8c9
kirill <kirill2004pro1000@mail.ru>
2022-04-27 23:07:43 +1000

    fix

---
+++ webpack.config.js
@@ -0,0 +1,50 @@
+const path = require('path')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const HTMLWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+    // мод
+    mode: "development",
+    // входной файл
+    entry: "./src/index.jsx",
+    resolve: {
+        extensions: ['.js', '.json', '.jsx'],
+    },
+    output: {
+        // выходная папка
+        path: path.resolve(__dirname, 'dist'),
+        // названия выходных файлов
+        filename: "[name].[hash].js"
+    },
+    // порт сервера
+    devServer: {
+        port: 3000
+    },
+    // плагины
+    plugins: [
+        // создаёт html файл и переносит наш html файл
+        new HTMLWebpackPlugin({
+            template: "./src/index.html"
+        }),
+        // очищает старые файлыы
+        new CleanWebpackPlugin()
+    ],
+    // модули
+    module: {
+        rules: [
+            {
+                test: /\.(css|s(a|c)ss)$/,
+                use: ['style-loader', 'css-loader', 'sass-loader']
+            },
+            {
+                test: /\.png$/,
+                use: ['file-loader']
+            },
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: ["babel-loader"]
+            },
+        ]
+    }
+}
